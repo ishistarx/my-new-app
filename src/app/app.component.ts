d037be043a589fa3061515fbe879c6431f4d49ce
ishistarx <ishitabiswas2002@gmail.com>
2022-04-23 19:52:36 +0530

    JsonGetAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './post.service';
+   
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-new-app';
+export class AppComponent implements OnInit {
+  posts :any ;
+   
+  constructor(private service:PostService) {}
+   
+  ngOnInit() {
+      this.service.getPosts()
+        .subscribe(response => {
+          this.posts = response;
+        });
+  }
 }
